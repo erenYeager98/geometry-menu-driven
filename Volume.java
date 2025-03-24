10bd102d050ece6019425041650f303ba4a212a4
Aldrin Lijo <aldrinlijo04@gmail.com>
2025-03-24 12:08:05 +0530

    volume - lijo

---
+++ Volume.java
@@ -0,0 +1,19 @@
+class Volume {
+    private double length;
+    private double width;
+    private double height;
+
+    public Volume(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculateVolume_rectangle() {
+        return length * width * height;
+    }
+
+    public double caluculateVolume_cylinder() {
+        return Math.PI * Math.pow(length / 2, 2) * height;
+    }
+}
