342f1f6d05fc92849d6ac89047e5db5bc0ca9089
George-Paul-Tony-C <171569996+George-Paul-Tony-C@users.noreply.github.com>
2025-03-24 12:21:43 +0530

    Main created successfully

--- Perimeter.java
+++ Perimeter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Perimeter {
-    public static void main(String[] args) {
+    public void Calculate() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Choose an option:");
         System.out.println("1. Circle");
